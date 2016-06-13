8f9a6e2a199dfaed4109f4b932871dc396092be3
Paolo Pandini <pandini.paolo@icloud.com>
2016-06-13 18:36:19 +0200

    Completata prima fase

--- project/views.py
+++ project/views.py
@@ -1,28 +1,18 @@
-# blog.py - controller
-
-# imports
-from flask import Flask, render_template, request, session, \
-	flash, redirect, url_for, g
 import sqlite3
+
 from functools import wraps
 
- 
+from flask import Flask, flash, redirect, render_template, request, session, url_for
 
-# configuration
-DATABASE = 'blog.db'
-USERNAME = 'admin'
-PASSWORD = 'admin'
-SECRET_KEY = 'hard_to_guess'
+# config
 
+app = Flask(__name__) 
+app.config.from_object('_config')
 
-app = Flask(__name__)
+# helper functions
 
-# pulls in app configuration by looking for UPPERCASE variables
-app.config.from_object(__name__)
-
-# function used for connecting to the database
 def connect_db():
-	return sqlite3.connect(app.config['DATABASE'])
+	return sqlite3.connect(app.config['DATABASE_PATH'])
 
 def login_required(test): 
 	@wraps(test)
@@ -34,54 +24,23 @@ def login_required(test):
 			return redirect(url_for('login')) 
 	return wrap
 
+# route handlers
 
-
-
-@app.route('/', methods = ['GET','POST']) 
-def login():
-	error = None
-	if request.method == 'POST':
-		if request.form['username'] != app.config['USERNAME'] or request.form['password'] != app.config['PASSWORD']:
-			error = 'Invalid Credentials. Please try again.' 
-		else:
-			session['logged_in'] = True
-			return redirect(url_for('main'))
-	return render_template('login.html', error=error)
-
-
-@app.route('/main')
-@login_required 
-def main():
-	g.db = connect_db()
-	cur = g.db.execute('select * from posts')
-	posts = [dict(title=row[0], post=row[1]) for row in
-		cur.fetchall()] 
-	g.db.close()
-	return render_template('main.html', posts=posts)
-
-@app.route('/add', methods=['POST']) 
-@login_required
-def add():
-	title = request.form['title'] 
-	post = request.form['post'] 
-	if not title or not post:
-		flash("All fields are required. Please try again.")
-		return redirect(url_for('main')) 
-	else:
-		g.db = connect_db()
-		g.db.execute('insert into posts (title, post) values (?,?)', [request.form['title'], request.form['post']])
-		g.db.commit()
-		g.db.close()
-		flash('New entry was successfully posted!') 
-		return redirect(url_for('main'))
-
-
-@app.route('/logout') 
-def logout():
+@app.route('/logout/')
+def logout(): 
 	session.pop('logged_in', None) 
-	flash('You were logged out') 
+	flash('Goodbye!')
 	return redirect(url_for('login'))
 
-
-if __name__ == '__main__': 
-	app.run(debug=True)
+@app.route('/', methods=['GET', 'POST']) 
+def login():
+	if request.method == 'POST':
+		if request.form['username'] != app.config['USERNAME'] \
+			or request.form['password'] != app.config['PASSWORD']:
+			error = 'Invalid Credentials. Please try again.'
+			return render_template('login.html', error=error) 
+		else:
+			session['logged_in'] = True 
+			flash('Welcome!')
+			return redirect(url_for('tasks'))
+	return render_template('login.html')
